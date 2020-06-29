50bbb9fe0121feb2e9b48e41f0597569d226bd52
rafaelguerreroclemente <rafa.guerrero@gmail.com>
2020-06-29 15:50:04 -0400

    Logo

---
+++ Assets/Scripts/UI/TapToNextSceen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TapToNextSceen : MonoBehaviour {
+	void Start() {
+        Button btn = this.gameObject.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick() {
+		Debug.Log("Clicked!!!");
+
+        //SceneManager.LoadScene("ChooseCharacter", LoadSceneMode.Single);  
+	}
+}
