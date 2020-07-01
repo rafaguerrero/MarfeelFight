c2e83d97ab6156898f0ca2e0b4252b8b3e742800
rafaelguerreroclemente <rafa.guerrero@gmail.com>
2020-07-01 13:35:25 -0400

    Update

--- Assets/Scripts/UI/TapToNextSceen.cs
+++ Assets/Scripts/UI/TapToNextSceen.cs
@@ -4,14 +4,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TapToNextSceen : MonoBehaviour {
+	public string nextSceenName;
+
 	void Start() {
         Button btn = this.gameObject.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
 	}
 
 	void TaskOnClick() {
-		Debug.Log("Clicked!!!");
-
-        //SceneManager.LoadScene("ChooseCharacter", LoadSceneMode.Single);  
+		SceneManager.LoadScene(nextSceenName, LoadSceneMode.Single);  
 	}
 }
